72190a8dcd258b375f7455486c7b15eb805b8909
unknown <farrel.ahmad.zaidan@gmail.com>
2024-02-10 14:16:02 +0700

    add store in sales controller

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -18,4 +18,29 @@ class SalesController extends Controller
         $users = User::all();
         return response()->json($users);
     }
+
+    /**
+     * Store a newly created user in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'user_level' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->user_level = $request->user_level;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password); 
+        $user->save();
+
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SalesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', 'App\Http\Controllers\SalesController@index');
+Route::get('/sales', [SalesController::class, 'index']);
+Route::post('/sales_add', [SalesController::class, 'store']);
 
 
 
